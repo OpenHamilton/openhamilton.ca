0a36cff0c3f23c727a6be72d2f96ac351994e70c
Brian <drakain@gmail.com>
2011-11-29 23:11:12 -0500

    Added template class, working on site page content.

---
+++ templates/templates.php
@@ -0,0 +1,70 @@
+<?php
+
+//Class that manages templates.
+class Template {
+    private $fileData;
+    private $customData;
+
+    public function Template($fileName, $data) {
+        $this->loadFile($fileName);
+        $this->setData($data);
+    }
+
+    //Load data from filename
+    public function loadFile($fileName) {
+        $this->fileData = file_get_contents($fileName);
+    }
+
+    //Set data to populate template
+    public function setData($data) {
+        $this->customData = $data;
+    }
+    
+    //Merge template with data
+    public function mergeData() {
+        $template = $this->fileData;
+        foreach ($this->customData as $key => $val) {
+            $template = str_replace($key, $val, $template);
+        }
+        return $template;
+    }
+
+    //Set individual value after constructor call.
+    public function setValue($value, $data) {
+        $this->customData[$value] = $data;
+    }
+}
+
+class MainPage extends Template {
+    
+    public function MainPage($data) {
+        parent::Template("./templates/main.html", $data);
+    }
+    
+}
+
+class DetailsPage extends Template {
+    
+    public function DetailsPage($data) {
+        parent::Template("./templates/details.html", $data);
+    }
+    
+}
+
+class TextBuilders {
+    public function standardBlock($heading, $paragraph){
+        $heading = htmlentities($heading);
+        $paragraph = htmlentities($paragraph);
+        $output = "";
+        $output .= "<h2>{$heading}</h2>";
+        $output .= "<p>{$paragraph}</p>";
+        return $output;
+    }
+    public function standardDivBlock($heading, $paragraph){
+        $output = "";
+        $output .= "<div>";
+        $output .= $this->standardBlock($heading, $paragraph);
+        $output .= "</div>";
+        return $output;
+    }
+}
